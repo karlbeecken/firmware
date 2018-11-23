315adecad7cafba2196a1a44e7ac1be669e3dedb
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-11-23 19:48:08 +0100

    update to HEAD of OpenWRT-master (22. Nov 2018)
    952b11766c rpcd: update to latest Git head
    4105555115 rpcd: update to latest Git head
    2ac1a57677 base-files: fix unkillable processes after restart
    3a1978bbb4 kernel: bump 3.18 to 3.18.126
    02e16e9e82 kernel: bump 4.14 to 4.14.82
    e1debc557c kernel: bump 4.9 to 4.9.138
    d3381b5a64 bcm53xx: request Seama parser on DIR-885L directly
    dab5a93086 kernel: add DT binding support to the Seama parser
    1dcaef8410 glibc: update to latest 2.27 commit
    0191a06985 mt76: update to the latest version
    8184bdddc4 mt76: update to the latest version
    4585cd3bdf kernel: move diff adding run_parsers_by_type() between mtd patches
    4d0431801d kernel: don't export parse_mtd_partitions_by_type() in 4.9 and 4.14
    48d8d46d33 wireguard: bump to 0.0.20181119
    9b8a955358 kernel: drop mtd_pad_erasesize() helper from 4.9 and 4.14
    8e409f476b netifd: update to latest git HEAD
    dd02a19ff5 ar71xx: fix TP-Link Archer C7 v5 switch LEDs
    251c350727 mt76: update to the latest version
    86fc443f8b mt76: update to the latest version
    bf52c968e8 wireguard: bump to 0.0.20181115
    59ff8687c7 util-linux: Update to 2.33
    15f16bf05b ar71xx: add support for Gainstrong MiniBox V3.2
    c6a1bcac16 kernel: fix ubifs loosing O_TMPFILE data after power cut
    80526d2fed bison: update to 3.2.1
    3a6bddd7f7 hostapd: add utf8_ssid flag & enable as default
    e14dc93073 kernel: bump 4.14 to 4.14.81
    7e20e4ab96 kernel: bump 4.9 to 4.9.137
    f7a406deaf kernel: bump 3.18 to 3.18.125

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=042d68a19593ac796098845366a235f5465816da
+OPENWRT_COMMIT=952b11766cd83898cf8f9626b75141eac6d4ad1a
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
