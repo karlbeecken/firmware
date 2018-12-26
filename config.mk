c49e62187298a3f54d26aaedf7d559440189a5b3
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-12-26 23:50:41 +0100

    use OpenWrt with native RB750Gr3-boot
    from branch SAm0815_experimental

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 OPENWRT_SRC=https://github.com/freifunk-berlin/openwrt-core.git
-OPENWRT_COMMIT=77429a820f8efff25c8e3c3e3579f636eff58fbc
+OPENWRT_COMMIT=d78dc898cb7c038a8f1bc749d346f5908273f447
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
