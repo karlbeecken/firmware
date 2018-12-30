73fd05342015715af00dc93e6b1db45fab2d7dd4
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-12-30 01:36:16 +0100

    config: switch to local fork of openwrt-repo

--- config.mk
+++ config.mk
@@ -2,7 +2,7 @@
 SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
-OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
+OPENWRT_SRC=https://github.com/freifunk-berlin/openwrt-core.git
 OPENWRT_COMMIT=77429a820f8efff25c8e3c3e3579f636eff58fbc
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
