2ea4f039ba332faeffbac8eb8db5d62eff516743
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-12-30 19:47:45 +0100

    add preliminary support for TPLink ArcherC50v4

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 OPENWRT_SRC=https://github.com/freifunk-berlin/openwrt-core.git
-OPENWRT_COMMIT=d78dc898cb7c038a8f1bc749d346f5908273f447
+OPENWRT_COMMIT=c625de8a2d34341af20e082855774d40088e40cf
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
