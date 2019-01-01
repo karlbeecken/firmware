bdeeec414119e4b210aa0b2d9a7f5f96076a33f8
Sven Roederer <freifunk@it-solutions.geroedel.de>
2019-01-01 17:21:12 +0100

    update to HEAD of OpenWRT-master (1. Jan 2019)
    6d6127e537 brcm63xx: fix gpio hogs on gpio/pinctrl nodes
    33bff49a31 brcm63xx: VH4032N: add the SPROM fixups
    d09561f73d brcm63xx: fix bcm6348 pinmux group selection
    a01568fbd3 brcm63xx: remove broken DSP platform device code
    d3bf5ff9bc opkg: drop argument from check_signature in opkg.conf
    7c1332d95f ramips: consolidate seama image build code
    de42466528 ar71xx: sync seama image build code with ramips
    fd35c5b205 build: move seama commands to image-commands.mk
    abc7ed2c58 ath79: add support for D-Link DIR-859 A1
    06b1a6e885 ath79: fix OCEDO Koala ethernet configuration
    213c0e78fa iwinfo: fix PKG_MIRROR_HASH
    af28d8a539 ath79: add support for GL.iNet GL-AR750S
    98f826fdc0 ath79: use caldata patching for Archer C58/C59
    9601d94138 ath79: add support for TP-Link TL-WR841N/ND v8
    31dc0fbca3 ath79: Fix system LED on TP-Link WR740/741 v4
    ab12913676 ath79: Fix UBNT Unifi AC LEDs
    caf306ce91 ath79: add support for ELECOM WRC-1750GHBK2-I/C
    8ba76d6e74 ath79: fix boardname of GL.iNet GL-AR150
    c2ecec07cc ath79: Add GL.iNet AR150 LED triggers
    cdbf2de777 ath79: Add support for TP-Link WR810N
    6e78d546d1 ath79: fix boardname of GL.iNet GL-AR300M
    48745221a8 ath79: add status LED on GL.iNet AR300M
    9ebb44aef4 ath79: add support for EnGenius EWS511AP
    d03aae1a09 ath79: add support for TP-Link Archer C6 v2
    3dfc07d186 ath79: add support for COMFAST CF-E110N
    1c39dcc2bb ath79: tweak the position of factory partition for wr818
    28a5674e33 treewide: fix spaces vs. tabs

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 OPENWRT_SRC=https://github.com/freifunk-berlin/openwrt-core.git
-OPENWRT_COMMIT=c625de8a2d34341af20e082855774d40088e40cf
+OPENWRT_COMMIT=0ab12c023c013238df92c198986fd432bece2f8b
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
