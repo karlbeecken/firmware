97b885abb4898d9601d2aedd775c0aec77c3f27e
Sven Roederer <freifunk@it-solutions.geroedel.de>
2019-01-06 14:41:49 +0100

    update to OpenWrt to HEAD of master (5. Jan 2019)

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 OPENWRT_SRC=https://github.com/freifunk-berlin/openwrt-core.git
-OPENWRT_COMMIT=0ab12c023c013238df92c198986fd432bece2f8b
+OPENWRT_COMMIT=4213e561f7ff5a5c418e24457a51624e3125341e
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
