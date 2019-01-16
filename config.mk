e93f0d1ee1d60de583b788790e4cffff6bd4d554
Sven Roederer <freifunk@it-solutions.geroedel.de>
2019-01-16 23:33:38 +0100

    Update to HEAD of OpenWrt-master
    634c733065 ipq40xx: copy Fritz4040 UBoot to STAGING_DIR_IMAGE
    488af51f81 mac80211: add ABI_VERSION to fix rebuild of dependent packages after upgrades
    b3664bc702 glibc: update to latest 2.27 commit (BZ #24018)
    6b9bdbd493 ramips: add two-way hashing scheme for MT7621
    93c35bfa21 ramips: whitespace cleanup inside hnat driver
    a18f68ff30 kernel: bump 3.18 to 3.18.131
    30d518bf16 kernel: bump 4.9 to 4.9.148
    f56a4e809b kernel: bump 4.14 to 4.14.91
    4ef4ca4581 toolchain/gdb: bump to 8.2.1
    2d4f09404d gdb: bump to 8.2.1
    630d8b87a5 cns3xxx: use actual size reads for PCIe
    529c95cc15 mac80211: brcmfmac: fix use-after-free & possible NULL pointer dereference
    ff62e83211 bison: update to 3.2.4
    df57d71a1a mac80211: brcmfmac: trivial patches rename to use v5.0
    adc8b374e3 mac80211: brcmfmac: backport fixes from the 5.0-rc1
    94561333cd brcm47xx: backport a final/accepted Netgear WNDR3400v3 patch
    7175b1356e brcm47xx: backport MIPS fix adding struct device for the SoC
    5a59e2c059 samba36: append config from /var/run/config/ for runtime shares
    ef1efa756e samba36: add package with hotplug.d script for auto sharing
    34696ce25e kernel: backport bcma patches that improve printing functions
    45ce9897c0 kernel: add bcma fix for subdevices DMA parameters
    70ffcb947c odhcp6c: update to latest git HEAD
    c5373a3f0c mt76: update to the latest version
    0e8d5ff0fc mt76: fix typo in version number
    e4088cb84b cryptodev-linux: Update to 1.10
    4bd9d331e5 octeon: fix typo in platform.sh
    7197115c25 tools/firmware-utils: fix sysupgrade typo in mkdapimg
    e42a227ee3 tools/firmware-utils: fix sysupgrade typo in mkdapimg2
    6be064c788 bzip2: fix hardening build
    bc89690f6e gdb: The signal definitions of musl and gdb collide
    0ea7160abe x86: Use kernel specific subtarget configurations
    f6e0ecdceb x86: Refresh kernel 4.14 configuration
    75fdf3ba01 x86: Add support for kernel 4.19
    ed2839ac41 kernel/modules: add kmod-pmbus-zl6100 module
    5de6aed42c mpc85xx: add support for Freescale (NXP) P2020RDB
    46d4c09bb6 mpc85xx: disable initramfs image in TL-WDR4900
    85be0f4c21 apm821xx: 4.14: sync config
    97e4311fca mpc85xx: add support for Sophos RED 15w Rev.1
    bd03255ec4 generic: ar8216: ar8327: kill warnings
    c7102a7699 kernel/modules: Fix build of kmod-pmbus
    c54e4747be kernel: MIPS: Add CPU option reporting to /proc/cpuinfo
    4ab689c137 malta: Refresh kernel 4.14 configuration
    403d36ef3d malta: Add support for kernel 4.19
    cfc9bde379 kernel/modules: Add missing config option
    48f81ee1d0 sunxi: Remove unneeded patches
    0057beab1b sunxi: Use kernel specific sub target configuration
    2eeb5d6aac sunxi: Refresh kernel 4.14 configuration
    900c6b1a42 sunxi: Add support for kernel 4.19
    4ed05b805b uboot-sunxi: Update to version 2018.11
    95d4c7c24b arm-trusted-firmware-sunxi: Replace with official ARM version
    d605831068 uboot-sunxi: update Theobroma A31 Pangolin support
    7a53fb40ca uboot-sunxi: disable AXP209 on Olimex A13 Olinuxino
    d560deb7f2 uboot-sunxi: Orange Pi Zero Plus: Fix SdCard detection
    3f0eb71dae busybox: add ALTERNATIVES for wget
    055cdab2bb uclient: add ALTERNATIVES for wget
    f4aa9d8839 firmware-utils: mkdlinkfw: cleanup code
    e428b127e7 firmware-utils: mkdlinkfw: create reproducible header
    0e78af22d9 firmware-utils: mkdlinkfw: fix error handling
    6a3f8b8818 build: remove duplicate mksercomfw image recipe
    01e95dc0dc firmware-utils: mkdlinkfw: fix build
    7a4075bd10 busybox: fix ALTERNATIVES alphabetical ordering
    8c6f00ef4f kernel: bump 4.14 to 4.14.93
    ba4fe412c4 elfutils: bump to 0.175
    6e104c63d6 kmod-sched-cake: bump to latest cake
    4029788ff3 odhcpd: update to latest git HEAD (FS#2020)

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 OPENWRT_SRC=https://github.com/freifunk-berlin/openwrt-core.git
-OPENWRT_COMMIT=bbe7ad5a2f0ba298dcc78fc9000cfff25c198a5a
+OPENWRT_COMMIT=4029788ff36a05a8731183269b675624269da84e
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
