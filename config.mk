80e63d6e840e5926b5ba0861ec621871dacbb9c4
Sven Roederer <freifunk@it-solutions.geroedel.de>
2019-01-17 00:28:55 +0100

    Update to HEAD of OpenWrt-master
    ad8c2d6099 tools: add zip utility
    18c9faa032 tools: zip: add option for reproducible archives
    26a016731d firmware-utils: mksercommfw: overhaul image creation

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 OPENWRT_SRC=https://github.com/freifunk-berlin/openwrt-core.git
-OPENWRT_COMMIT=4029788ff36a05a8731183269b675624269da84e
+OPENWRT_COMMIT=26a016731d1fc1e2bd6b5f8d3af0fdbd631363ff
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
