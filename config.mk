3cb2f29c6ae0ff4ec1a4938aaf13b76011f7d180
Sven Roederer <freifunk@it-solutions.geroedel.de>
2019-01-17 20:20:37 +0100

    Update to HEAD of OpenWrt-master
    5be22ef2fa kernel: bump 3.18 to 3.18.132
    63a2ed3ba5 kernel: bump 4.9 to 4.9.150
    7541d30c9c dnsmasq: backport latest pre2.81 patches
    76cc766521 odhcpd: fix onlink IA check (FS#2060)

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 OPENWRT_SRC=https://github.com/freifunk-berlin/openwrt-core.git
-OPENWRT_COMMIT=26a016731d1fc1e2bd6b5f8d3af0fdbd631363ff
+OPENWRT_COMMIT=76cc7665211266c810e59ebd74b810f69aaf8706
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
