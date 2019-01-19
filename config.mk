bcd516ea7d38b17b7abac374f4ff7e13de48f9fd
Sven Roederer <freifunk@it-solutions.geroedel.de>
2019-01-19 07:16:05 +0100

    Update to HEAD of OpenWrt-18.06
    61323d22c0 dropbear: fix dropbear startup issue
    eef6bd3393 bcm53xx: update pinctrl driver & use its new DT binding
    dbbba84ed4 bcm53xx: rename dts backport patches changing their prefixes
    5af6b7976f bcm53xx: backport DTS changes queued for the 4.21
    baa00b2182 brcm2708-gpu-fw: update to git HEAD
    806b570a35 ath10k: update QCA4019 firmware
    016a71a18a odhcpd: backport prefix filter/NETEV_ADDR6LIST_CHANGE event fixes
    afcdf099b7 kernel: fix f2fs on big endian machines
    8a9b0b95df apm821xx: switch MX60(W)'s recovery images to multi-image method
    62ddfaff32 odhcpd: filter routes based on prefix_filter
    c5c20f510a odhcpd: noop to fix PKG_SOURCE_DATE
    a8cc06c537 mac80211: brcmfmac: backport firmware loading changes & fix memory bugs
    b9dcf2de5f kernel: bump 4.9 to 4.9.148
    367ccf7a2d kernel: bump 4.14 to 4.14.91
    c3fef269d6 cns3xxx: use actual size reads for PCIe
    4c0e15fc16 base-files: install missing /etc/iproute2/ematch_map
    e0daa62af6 fstools: Install mount.hotplug and 10-fstab.defaults as 600
    d7275c0312 fstools: filter unknown action in mount.hotplug script
    3b4e779e93 fstools: update to git HEAD
    069fda746e fstools: update to the latest master branch
    0d6eaa0ba5 octeon: fix typo in platform.sh
    0cef35f096 tools/firmware-utils: fix sysupgrade typo in mkdapimg
    d58c6aedb6 tools/firmware-utils: fix sysupgrade typo in mkdapimg2
    f34ad1a8f0 mt76: update to the latest version
    cae263227f kernel: bump 4.9 to 4.9.150
    939fa07b04 kernel: bump 4.14 to 4.14.93
    abd0f7995e kmod-sched-cake: bump to latest cake
    7f98cd8d50 odhcpd: fix onlink IA check (FS#2060)

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=753531da249f4960b1088513665982322a2b6a97
+OPENWRT_COMMIT=7f98cd8d503e33ad43d6273d417474086bcb446d
 SET_BUILDBOT=env
 MAKE_ARGS=
