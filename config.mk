8ab8fef6c6b51c5d2d829c8042bd0dbb9f0edebd
Sven Roederer <freifunk@it-solutions.geroedel.de>
2019-01-22 00:10:05 +0100

    Update to HEAD of OpenWrt-18.06
    3603c2321d ramips: mt7621: fix 5GHz WiFi LED on ZBT WG3526

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=7f98cd8d503e33ad43d6273d417474086bcb446d
+OPENWRT_COMMIT=3603c2321d675b7086d9413a68bcfdf1da3cccb5
 SET_BUILDBOT=env
 MAKE_ARGS=
