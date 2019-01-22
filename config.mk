00574870bcc2c626c0584181b2a932f363a90651
Sven Roederer <freifunk@it-solutions.geroedel.de>
2019-01-22 12:56:20 +0100

    Update to HEAD of OpenWrt-master
    116d0f276d kernel: add DT binding support to the Edimax uImage parser
    f5e61350e7 ramips: fix firmware splitter for edimax based boards
    1bd18f2b5c opkg: update to latest Git head
    797e5c1c48 packages: set more explicit ABI_VERSION values
    2d9d57b9de build: rename .packagesubdirs to .packageauxvars
    e3d5b384aa build: expose ABI version in .packageauxvars
    e6bcf1e4ac build: add ABI_VERSION to binary package names
    1fdb4a7907 kernel: N_BADCLASS: fix macro to actually work - class e support
    351e5516eb nghttp2: bump to 1.36.0
    c8280e6e14 mac80211: add support for indicating missing tx A-MPDU length
    3ccc0fdd9c mt76: update to the latest version
    62dadcb86c bcm53xx: add support for Phicomm K3
    c594cdae0c kernel: bump 4.9 to 4.9.151
    3fe555c719 kernel: bump 4.14 to 4.14.94
    8c9b182db6 scripts: ipkg-remove: handle existing .ipk files without SourceName field

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 OPENWRT_SRC=https://github.com/freifunk-berlin/openwrt-core.git
-OPENWRT_COMMIT=27be78ef461af31015a6c9cfc334b3c5c69b3953
+OPENWRT_COMMIT=8c9b182db63fb8be6906cdf8df84e58c9c9daae7
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
